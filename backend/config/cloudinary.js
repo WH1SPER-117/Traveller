871f9ca42f04687627a9d21505be77a34f534911
Hasvin Kenula <131936695+WH1SPER-117@users.noreply.github.com>
2026-03-11 21:45:02 +0530

    Deploying

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -6,7 +6,5 @@ api_key: process.env.CLOUDINARY_API_KEY,
 api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-console.log("Cloud:", process.env.CLOUDINARY_CLOUD_NAME);
-console.log("Key:", process.env.CLOUDINARY_API_KEY);
 
 module.exports = cloudinary;
