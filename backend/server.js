d3688af4ff047ad38f2fdbbafe630e99f753bde8
Hasvin Kenula <131936695+WH1SPER-117@users.noreply.github.com>
2026-03-11 12:09:11 +0530

    backend skeleton done

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+
+
+app.get("/", (req, res) => {
+  res.send("API running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
